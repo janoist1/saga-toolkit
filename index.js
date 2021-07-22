8739be1fe9d315b584f406498e166a095317dff8
Istvan Jano <janoist1@gmail.com>
2021-07-22 13:31:29 +0200

    Async processing of actions

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var _toolkit = require("@reduxjs/toolkit");
 
 var _effects = require("redux-saga/effects");
 
-var _marked2 = /*#__PURE__*/regeneratorRuntime.mark(putAsync);
+var _marked3 = /*#__PURE__*/regeneratorRuntime.mark(putAsync);
 
 function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
 
@@ -75,183 +75,226 @@ var takeAsync = function takeAsync(_ref2) {
     }
 
     return (0, _effects.fork)( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
-      var task, _loop;
+      var _marked2, queue, processQueue, processor, _processor, action, _action$meta, requestId;
 
-      return regeneratorRuntime.wrap(function _callee$(_context3) {
+      return regeneratorRuntime.wrap(function _callee$(_context4) {
         while (1) {
-          switch (_context3.prev = _context3.next) {
+          switch (_context4.prev = _context4.next) {
             case 0:
-              _loop = /*#__PURE__*/regeneratorRuntime.mark(function _loop() {
-                var _marked, action, _action$meta, requestId, request, deferred, _deferred, resolve, reject, wrap;
+              processQueue = function _processQueue() {
+                var task, _loop;
 
-                return regeneratorRuntime.wrap(function _loop$(_context2) {
+                return regeneratorRuntime.wrap(function processQueue$(_context3) {
                   while (1) {
-                    switch (_context2.prev = _context2.next) {
+                    switch (_context3.prev = _context3.next) {
                       case 0:
-                        wrap = function _wrap(saga) {
-                          var _len2,
-                              args,
-                              _key2,
-                              result,
-                              _args = arguments;
+                        _loop = /*#__PURE__*/regeneratorRuntime.mark(function _loop() {
+                          var _marked, action, requestId, request, deferred, _deferred, resolve, reject, wrap;
 
-                          return regeneratorRuntime.wrap(function wrap$(_context) {
+                          return regeneratorRuntime.wrap(function _loop$(_context2) {
                             while (1) {
-                              switch (_context.prev = _context.next) {
+                              switch (_context2.prev = _context2.next) {
                                 case 0:
-                                  _context.prev = 0;
+                                  wrap = function _wrap(saga) {
+                                    var _len2,
+                                        args,
+                                        _key2,
+                                        _args = arguments;
 
-                                  for (_len2 = _args.length, args = new Array(_len2 > 1 ? _len2 - 1 : 0), _key2 = 1; _key2 < _len2; _key2++) {
-                                    args[_key2 - 1] = _args[_key2];
-                                  }
+                                    return regeneratorRuntime.wrap(function wrap$(_context) {
+                                      while (1) {
+                                        switch (_context.prev = _context.next) {
+                                          case 0:
+                                            _context.prev = 0;
 
-                                  _context.next = 4;
-                                  return _effects.call.apply(void 0, [saga].concat(args));
+                                            for (_len2 = _args.length, args = new Array(_len2 > 1 ? _len2 - 1 : 0), _key2 = 1; _key2 < _len2; _key2++) {
+                                              args[_key2 - 1] = _args[_key2];
+                                            }
 
-                                case 4:
-                                  result = _context.sent;
-                                  resolve(result);
-                                  _context.next = 11;
-                                  break;
+                                            _context.t0 = resolve;
+                                            _context.next = 5;
+                                            return _effects.call.apply(void 0, [saga].concat(args));
 
-                                case 8:
-                                  _context.prev = 8;
-                                  _context.t0 = _context["catch"](0);
-                                  reject(_context.t0);
+                                          case 5:
+                                            _context.t1 = _context.sent;
+                                            (0, _context.t0)(_context.t1);
+                                            _context.next = 12;
+                                            break;
 
-                                case 11:
-                                  _context.prev = 11;
-                                  _context.next = 14;
-                                  return (0, _effects.cancelled)();
+                                          case 9:
+                                            _context.prev = 9;
+                                            _context.t2 = _context["catch"](0);
+                                            reject(_context.t2);
 
-                                case 14:
-                                  if (!_context.sent) {
-                                    _context.next = 16;
+                                          case 12:
+                                            _context.prev = 12;
+                                            _context.next = 15;
+                                            return (0, _effects.cancelled)();
+
+                                          case 15:
+                                            if (!_context.sent) {
+                                              _context.next = 17;
+                                              break;
+                                            }
+
+                                            reject('Saga cancelled');
+
+                                          case 17:
+                                            return _context.finish(12);
+
+                                          case 18:
+                                          case "end":
+                                            return _context.stop();
+                                        }
+                                      }
+                                    }, _marked, null, [[0, 9, 12, 18]]);
+                                  };
+
+                                  _marked = /*#__PURE__*/regeneratorRuntime.mark(wrap);
+                                  action = queue.shift();
+                                  requestId = action.meta.requestId;
+                                  request = requests[requestId];
+                                  deferred = void 0;
+
+                                  if (request) {
+                                    _context2.next = 12;
                                     break;
                                   }
 
-                                  reject('Saga cancelled');
+                                  _context2.next = 9;
+                                  return new Promise(function (onAdd) {
+                                    requests[requestId] = {
+                                      onAdd: onAdd
+                                    };
+                                  });
 
-                                case 16:
-                                  return _context.finish(11);
+                                case 9:
+                                  deferred = _context2.sent;
+                                  _context2.next = 13;
+                                  break;
 
-                                case 17:
+                                case 12:
+                                  deferred = request.deferred;
+
+                                case 13:
+                                  _deferred = deferred, resolve = _deferred.resolve, reject = _deferred.reject;
+                                  _context2.t0 = latest && task;
+
+                                  if (!_context2.t0) {
+                                    _context2.next = 19;
+                                    break;
+                                  }
+
+                                  _context2.next = 18;
+                                  return (0, _effects.cancelled)(task);
+
+                                case 18:
+                                  _context2.t0 = !_context2.sent;
+
+                                case 19:
+                                  if (!_context2.t0) {
+                                    _context2.next = 22;
+                                    break;
+                                  }
+
+                                  _context2.next = 22;
+                                  return (0, _effects.cancel)(task);
+
+                                case 22:
+                                  if (!(aggregate && task)) {
+                                    _context2.next = 26;
+                                    break;
+                                  }
+
+                                  requests[task.requestId].deferred.promise.then(resolve)["catch"](reject);
+                                  _context2.next = 30;
+                                  break;
+
+                                case 26:
+                                  _context2.next = 28;
+                                  return _effects.fork.apply(void 0, [wrap, saga].concat(_toConsumableArray(args.concat(action))));
+
+                                case 28:
+                                  task = _context2.sent;
+                                  task.requestId = requestId;
+
+                                case 30:
+                                  requests[task.requestId].deferred.promise["finally"](function () {
+                                    delete requests[requestId];
+                                  });
+
+                                case 31:
                                 case "end":
-                                  return _context.stop();
+                                  return _context2.stop();
                               }
                             }
-                          }, _marked, null, [[0, 8, 11, 17]]);
-                        };
-
-                        _marked = /*#__PURE__*/regeneratorRuntime.mark(wrap);
-                        _context2.next = 4;
-                        return (0, _effects.take)(patternOrChannel);
-
-                      case 4:
-                        action = _context2.sent;
-                        _action$meta = action === null || action === void 0 ? void 0 : action.meta, requestId = _action$meta.requestId;
-
-                        if (requestId) {
-                          _context2.next = 8;
-                          break;
-                        }
-
-                        throw Error('Non-saga action');
-
-                      case 8:
-                        request = requests[requestId];
-                        deferred = void 0;
-
-                        if (request) {
-                          _context2.next = 16;
-                          break;
-                        }
-
-                        _context2.next = 13;
-                        return new Promise(function (onAdd) {
-                          requests[requestId] = {
-                            onAdd: onAdd
-                          };
+                          }, _loop);
                         });
 
-                      case 13:
-                        deferred = _context2.sent;
-                        _context2.next = 17;
+                      case 1:
+                        if (!queue.length) {
+                          _context3.next = 5;
+                          break;
+                        }
+
+                        return _context3.delegateYield(_loop(), "t0", 3);
+
+                      case 3:
+                        _context3.next = 1;
                         break;
 
-                      case 16:
-                        deferred = request.deferred;
-
-                      case 17:
-                        _deferred = deferred, resolve = _deferred.resolve, reject = _deferred.reject;
-                        _context2.t0 = latest && task;
-
-                        if (!_context2.t0) {
-                          _context2.next = 23;
-                          break;
-                        }
-
-                        _context2.next = 22;
-                        return (0, _effects.cancelled)(task);
-
-                      case 22:
-                        _context2.t0 = !_context2.sent;
-
-                      case 23:
-                        if (!_context2.t0) {
-                          _context2.next = 26;
-                          break;
-                        }
-
-                        _context2.next = 26;
-                        return (0, _effects.cancel)(task);
-
-                      case 26:
-                        if (!(aggregate && task)) {
-                          _context2.next = 30;
-                          break;
-                        }
-
-                        requests[task.requestId].deferred.promise.then(resolve)["catch"](reject);
-                        _context2.next = 34;
-                        break;
-
-                      case 30:
-                        _context2.next = 32;
-                        return _effects.fork.apply(void 0, [wrap, saga].concat(_toConsumableArray(args.concat(action))));
-
-                      case 32:
-                        task = _context2.sent;
-                        task.requestId = requestId;
-
-                      case 34:
-                        requests[task.requestId].deferred.promise["finally"](function () {
-                          delete requests[requestId];
-                        });
-
-                      case 35:
+                      case 5:
                       case "end":
-                        return _context2.stop();
+                        return _context3.stop();
                     }
                   }
-                }, _loop);
-              });
+                }, _marked2);
+              };
 
-            case 1:
+              _marked2 = /*#__PURE__*/regeneratorRuntime.mark(processQueue);
+              queue = [];
+
+            case 3:
               if (!true) {
-                _context3.next = 5;
+                _context4.next = 17;
                 break;
               }
 
-              return _context3.delegateYield(_loop(), "t0", 3);
+              _context4.next = 6;
+              return (0, _effects.take)(patternOrChannel);
 
-            case 3:
-              _context3.next = 1;
+            case 6:
+              action = _context4.sent;
+              _action$meta = action === null || action === void 0 ? void 0 : action.meta, requestId = _action$meta.requestId;
+
+              if (requestId) {
+                _context4.next = 10;
+                break;
+              }
+
+              throw Error('Non-saga action');
+
+            case 10:
+              queue.push(action);
+
+              if ((_processor = processor) !== null && _processor !== void 0 && _processor.isRunning()) {
+                _context4.next = 15;
+                break;
+              }
+
+              _context4.next = 14;
+              return (0, _effects.fork)(processQueue);
+
+            case 14:
+              processor = _context4.sent;
+
+            case 15:
+              _context4.next = 3;
               break;
 
-            case 5:
+            case 17:
             case "end":
-              return _context3.stop();
+              return _context4.stop();
           }
         }
       }, _callee);
@@ -271,26 +314,26 @@ var takeAggregateAsync = takeAsync({
 exports.takeAggregateAsync = takeAggregateAsync;
 
 function putAsync(action) {
-  return regeneratorRuntime.wrap(function putAsync$(_context4) {
+  return regeneratorRuntime.wrap(function putAsync$(_context5) {
     while (1) {
-      switch (_context4.prev = _context4.next) {
+      switch (_context5.prev = _context5.next) {
         case 0:
-          _context4.t0 = _toolkit.unwrapResult;
-          _context4.next = 3;
+          _context5.t0 = _toolkit.unwrapResult;
+          _context5.next = 3;
           return (0, _effects.put)(action);
 
         case 3:
-          _context4.next = 5;
-          return _context4.sent;
+          _context5.next = 5;
+          return _context5.sent;
 
         case 5:
-          _context4.t1 = _context4.sent;
-          return _context4.abrupt("return", (0, _context4.t0)(_context4.t1));
+          _context5.t1 = _context5.sent;
+          return _context5.abrupt("return", (0, _context5.t0)(_context5.t1));
 
         case 7:
         case "end":
-          return _context4.stop();
+          return _context5.stop();
       }
     }
-  }, _marked2);
+  }, _marked3);
 }
--- sagaToolkit.js
+++ sagaToolkit.js
@@ -41,55 +41,73 @@ export const createSagaAction = type => {
 
 const takeAsync = ({ latest = false, aggregate = false }) => (patternOrChannel, saga, ...args) =>
   fork(function* () {
-    let task
+    const queue = []
+
+    function* processQueue() {
+      let task
+
+      while (queue.length) {
+        const action = queue.shift()
+        const { requestId } = action.meta
+        const request = requests[requestId]
+        let deferred
+
+        if (!request) {
+          deferred = yield (new Promise(onAdd => {
+            requests[requestId] = { onAdd }
+          }))
+        } else {
+          deferred = request.deferred
+        }
+
+        const { resolve, reject } = deferred
+
+        if (latest && task && !(yield cancelled(task))) {
+          yield cancel(task)
+        }
+
+        function* wrap(saga, ...args) {
+          try {
+            resolve(yield call(saga, ...args))
+          } catch (error) {
+            reject(error)
+          } finally {
+            if (yield cancelled()) {
+              reject('Saga cancelled')
+            }
+          }
+        }
+
+        if (aggregate && task) {
+          requests[task.requestId].deferred.promise.then(resolve).catch(reject)
+        } else {
+          task = yield fork(wrap, saga, ...args.concat(action))
+          task.requestId = requestId
+        }
+
+        requests[task.requestId].deferred.promise.finally(() => {
+          delete requests[requestId]
+        })
+      }
+    }
+
+
+    let processor
 
     while (true) {
       const action = yield take(patternOrChannel)
+
       const { requestId } = action?.meta
 
       if (!requestId) {
         throw Error('Non-saga action')
       }
 
-      const request = requests[requestId]
-      let deferred
+      queue.push(action)
 
-      if (!request) {
-        deferred = yield (new Promise(onAdd => {
-          requests[requestId] = { onAdd }
-        }))
-      } else {
-        deferred = request.deferred
+      if (!processor?.isRunning()) {
+        processor = yield fork(processQueue)
       }
-
-      const { resolve, reject } = deferred
-
-      if (latest && task && !(yield cancelled(task))) {
-        yield cancel(task)
-      }
-
-      function* wrap(saga, ...args) {
-        try {
-          const result = yield call(saga, ...args)
-
-          resolve(result)
-        } catch (error) {
-          reject(error)
-        } finally {
-          if (yield cancelled()) {
-            reject('Saga cancelled')
-          }
-        }
-      }
-
-      if (aggregate && task) {
-        requests[task.requestId].deferred.promise.then(resolve).catch(reject)
-      } else {
-        task = yield fork(wrap, saga, ...args.concat(action))
-        task.requestId = requestId
-      }
-
-      requests[task.requestId].deferred.promise.finally(() => { delete requests[requestId] })
     }
   })
 
